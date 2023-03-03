ac6bb046b18692878d514a9af28dcd8157086046
ads-b791 <ads-b@i.ua>
2023-03-03 20:20:35 +0200

    проведен рефакторинг

--- main.js
+++ main.js
@@ -5,47 +5,68 @@ const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
 const header = document.querySelector('.header');
 
-form.onsubmit = function (e) {
+function removeCard() {
+	const prevCard = document.querySelector('.card');
+	if (prevCard) prevCard.remove();
+}
+
+
+function showError(errorMessage) {
+	const html = `<div class="card">${errorMessage}</div>`
+	header.insertAdjacentHTML('afterend', html);
+}
+
+function showCard(name, country, temp, icon, condition) {
+	const html =
+		`<div class="card">
+				<h2 class="card-city">${name}<span>${country}</span></h2>
+	
+				<div class="card-weather">
+					<div class="card-value">${temp}<sup>°c</sup></div>
+					<img class="card-img" src="https:${icon}" alt="Weather">
+				</div>
+	
+				<div class="card-description">${condition}</div>
+				</div>`
+	header.insertAdjacentHTML('afterend', html);
+}
+
+async function getWeather(city) {
+
+	const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+	const response = await fetch(url);
+	const data = await response.json();
+
+	return data;
+}
+
+form.onsubmit = async function (e) {
 	e.preventDefault();
 
 	let city = input.value.trim();
 
-	const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+	const data = await getWeather(city);
 
+	if (data.error) {
 
-	fetch(url).then((response) => {
-		return response.json()
-	}).then((data) => {
+		removeCard();
 
-		if (data.error) {
-			const prevCard = document.querySelector('.card');
-			if (prevCard) prevCard.remove();
+		showError(data.error.message);
 
-			const html = `<div class="card">${data.error.message}</div>`
-			header.insertAdjacentHTML('afterend', html);
+	} else {
 
-		} else {
-			const prevCard = document.querySelector('.card');
-			if (prevCard) prevCard.remove();
+		removeCard();
 
-			const html =
-				`<div class="card">
-				<h2 class="card-city">${data.location.name}<span>${data.location.country}</span></h2>
-	
-				<div class="card-weather">
-					<div class="card-value">${data.current.temp_c}<sup>°c</sup></div>
-					<img class="card-img" src="https:${data.current.condition.icon}" alt="Weather">
-				</div>
-	
-				<div class="card-description">${data.current.condition.text}</div>
-				</div>`
-			header.insertAdjacentHTML('afterend', html);
+		showCard(
+			data.location.name,
+			data.location.country,
+			data.current.temp_c,
+			data.current.condition.icon,
+			data.current.condition.text
+		);
+	}
 
-		}
-
-
-
-	})
 
 }
 
@@ -67,3 +88,12 @@ form.onsubmit = function (e) {
 		//let img = document.querySelector('.card-img');
 		//console.log(img)
 		//img.src = `https:${icon}`;
+
+
+
+			//fetch(url).then((response) => {
+	//	return response.json()
+	//}).then((data) => {
+
+
+	//})
